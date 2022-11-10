7267015989eefaf39a66867cda4e7c42b023af52
Atqa Munzir <atqamz@gmail.com>
2022-11-10 23:13:39 +0700

    Initial commit

---
+++ punyaMasFahmi/client/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace SocketClientDemo
+{
+    class Mahasiswa
+    {
+        public string NIM { get; set; }
+        public string Nama { get; set; }
+
+        public Mahasiswa(string nim, string nama)
+        {
+            NIM = nim;
+            Nama = nama;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecuteClient();
+        }
+
+        private static void ExecuteClient()
+        {
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11000);
+
+            Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                sender.Connect(localEndPoint);
+
+                Console.WriteLine("Socket connected to -> {0} ", sender.RemoteEndPoint?.ToString());
+
+                byte[] messageSent = Encoding.ASCII.GetBytes("Test Client<EOF>");
+                int byteSent = sender.Send(messageSent);
+
+                byte[] messageReceived = new byte[1024];
+
+                // receive mahasiswa object from server and print it
+                int byteRecv = sender.Receive(messageReceived);
+                string json = Encoding.ASCII.GetString(messageReceived, 0, byteRecv);
+                Mahasiswa mhs = JsonConvert.DeserializeObject<Mahasiswa>(json);
+                Console.WriteLine("NIM: {0}, Nama: {1}", mhs.NIM, mhs.Nama);
+
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException : {0}", se.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected exception : {0}", e.ToString());
+            }
+        }
+    }
+}
---
+++ punyaMasFahmi/server/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace SocketServerDemo
+{
+    class Mahasiswa
+    {
+        public string NIM { get; set; }
+        public string Nama { get; set; }
+
+        public Mahasiswa(string nim, string nama)
+        {
+            NIM = nim;
+            Nama = nama;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecuteServer();
+        }
+
+        static void ExecuteServer()
+        {
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11000);
+
+            Socket listener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting connection ... ");
+
+                    Socket clientSocket = listener.Accept();
+                    byte[] bytes = new byte[1024];
+                    string data = "";
+
+                    while (true)
+                    {
+                        int numByte = clientSocket.Receive(bytes);
+
+                        data += Encoding.ASCII.GetString(bytes, 0, numByte);
+
+                        if (data.IndexOf("<EOF>") > -1)
+                            break;
+                    }
+
+                    // make new mahasiswa object and send to client
+                    Mahasiswa mhs = new Mahasiswa("5220600014", "Fahmi");
+                    string json = JsonConvert.SerializeObject(mhs);
+                    byte[] message = Encoding.ASCII.GetBytes(json);
+                    clientSocket.Send(message);
+
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        // method to send message to client
+        public static void Send(Socket handler, String data)
+        {
+            byte[] byteData = Encoding.ASCII.GetBytes(data);
+
+            handler.Send(byteData);
+        }
+    }
+}
